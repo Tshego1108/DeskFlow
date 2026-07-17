744009df116aeb21e4f5867b2d41727eb52e1a27
Tshego1108 <ptselahle@gmail.com>
2026-07-17 14:32:42 +0200

    Delete src directory

--- src/middleware/errorHandler.js
+++
@@ -1,5 +0,0 @@
-exports.errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  const status = err.status || 500;
-  res.status(status).json({message: err.message || 'Internal Server Error'});
-};
--- src/middleware/roles.js
+++
@@ -1,5 +0,0 @@
-exports.authorizeRoles = (...allowedRoles) => (req, res, next) => {
-  if (!req.user) return res.status(401).json({message: 'Not authenticated'});
-  if (!allowedRoles.includes(req.user.role)) return res.status(403).json({message: 'Forbidden'});
-  next();
-};
